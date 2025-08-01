6de9ee15c9a646d0db81e2ac88ece550c912735d
Matias Charriere <matias@giantswarm.io>
2025-08-01 12:22:30 +0200

    handle listmaps error
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

--- cilium/cilium.go
+++ cilium/cilium.go
@@ -62,9 +62,14 @@ func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
 // Collect implements the Collect method of the Collector interface.
 func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 	// TODO: Implement metric collection
-	policyMaps, _ := c.listAllMaps()
+	policyMaps, err := c.listAllMaps()
+	if err != nil {
+		c.logger.Log("level", "error", "message", "failed to list all maps", "error", err)
+		return
+	}
+
 	for _, policyMap := range policyMaps {
 		c.policyMaps.WithLabelValues(policyMap.EndpointID).Set(float64(policyMap.Size))
 	}
-	c.policyMaps.Collect(ch)
+
 }
